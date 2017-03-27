7d7f12cc15b01cdd0635e71ef10cca5f5ff1656f
Willem Wollebrants <willem@studiow.be>
2017-03-27 11:40:51 +0200

    Document null parameter

--- src/Identity/Uuid.php
+++ src/Identity/Uuid.php
@@ -10,16 +10,21 @@ use InvalidArgumentException;
 
 class Uuid extends NativeString
 {
+    /**
+     * Uuid constructor. Pass null to create a new, random ID
+     * @param string|null $value
+     */
     public function __construct(string $value = null)
     {
         if (is_null($value)) {
             $value = RamseyUuid::uuid4()->toString();
         }
-        $pattern = '#^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$#';
 
+        $pattern = '#^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$#';
         if (!preg_match($pattern, $value)) {
             throw new InvalidArgumentException(sprintf("Invalid UUID: %s", $value));
         }
+
         parent::__construct($value);
     }
 }
